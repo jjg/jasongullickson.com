e6bb52628e953bfb2a58de71c23a7e5e1d926a06
Jason J. Gullickson <mr@jasongullickson.com>
2022-03-31 10:01:44 -0500

    Make middleware return 20MB of data.

--- pages/api/nmet/a/_middleware.ts
+++ pages/api/nmet/a/_middleware.ts
@@ -5,7 +5,19 @@ export default function middleware(
   event: NextFetchEvent,
 ) {
 
-  const response = new Response('Hello from Edge, batch.');//new Response(JSON.stringify(data))
+  var bigStringSize = 20 * 1000 * 1000; // 20MB
+  var bigString = "";
+
+  for(var i=0;i<bigStringSize;i++){
+      bigString = bigString + "a";
+  }
+
+
+  //const response = new Response('Hello from Edge, batch.');//new Response(JSON.stringify(data))
+
+
+  const response = new Response(bigString); 
+
   response.headers.set('Cache-Control', 's-maxage=30')
 
   return response 
